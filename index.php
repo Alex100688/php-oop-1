57cc0b18da5d26c2531f9acd471246b76e352c86
Alex100688 <alessiozingaro2@gmail.com>
2023-01-09 17:39:41 +0100

    creata struttura

--- index.php
+++ index.php
@@ -3,11 +3,24 @@
         public $title;
         public $description;
         public $genre;
-        private $rating = 0;
+        private $rating = 10;
 
         public function __construct($_title, $_description, $_genre)
         {   $this -> title = $_title;
             $this -> description = $_description;
             $this -> genre = $_genre;
         }
+
+        public function setRating($vote){
+            if($vote === 10){
+                $this -> rating = "The film is very very good!";
+            } else if($vote >= 6){
+                $this -> rating = "The film is good";
+            } else if($vote <= 5){
+                $this -> rating ="The film is sufficent";
+            } else{
+                $this -> rating = "The film is realy ugly!!! ";
+            }
+        }
     }
+    
